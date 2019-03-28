291cff4d19925004e2281a9b35a83b137403dd92
Albin Remnestål <albin.remnestal@gmail.com>
2019-03-28 15:05:47 +0100

    deploy

---
+++ precache-manifest.5b80b990a2712866b157952e5f822b45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da78cb43edfff1086f31",
+    "url": "/static/js/main.f437de31.chunk.js"
+  },
+  {
+    "revision": "1ed850354407f73fd274",
+    "url": "/static/js/2.1640f7bc.chunk.js"
+  },
+  {
+    "revision": "da78cb43edfff1086f31",
+    "url": "/static/css/main.e3a7a720.chunk.css"
+  },
+  {
+    "revision": "75dd72c4a9dc4bab460404558575fc98",
+    "url": "/index.html"
+  }
+];
